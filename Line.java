3fc6c3c33b0a8f36539d4464a877afc4323c6630
hari karthick <harikarthickr20@gmail.com>
2026-04-21 15:50:28 +0530

    Add: calculate length of a line using two points

--- Line.java
+++ Line.java
@@ -1,3 +1,13 @@
 public class Line {
-    
+    Point p1;
+    Point p2;
+
+    public Line(Point p1, Point p2) {
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+
+    public double getLength() {
+        return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
+    }
 }
--- LineComparison.java
+++ LineComparison.java
@@ -1,5 +1,11 @@
 public class LineComparison {
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Computation Program");
+        Point p1 = new Point(2, 3);
+        Point p2 = new Point(6, 7);
+
+        Line line1 = new Line(p1, p2);
+
+        System.out.println("Length of line1 = " + line1.getLength());
     }
 }
